ab2573332eb825f03a9ca339007d2b11fb1eb524
Afroj_Mulani <mulaniafroj6@gmail.com>
2025-06-14 11:33:38 +0530

    Pages and Images ,Icons
    pages and images are required for the web are uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/Homepage/Homepage'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
 
 function App() {
   
 
   return (
     <>
-     <Homepage/>
+    <BrowserRouter>
+    <Header/>
+        <Routes>
+             <Route path='/' element={<Homepage/>}/>
+        </Routes>
+       <Footer/> 
+    </BrowserRouter>
     </>
   )
 }
